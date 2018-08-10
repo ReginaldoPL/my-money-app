2de0b7e99cc2fbacfb3f4920845b15dc08096aa0
reginaldoPL <reginaldo.pedro.de.lima@gmail.com>
2018-08-10 15:56:14 -0300

    correto até summary

--- src/api/billingCycle/billingCycleService.js
+++ src/api/billingCycle/billingCycleService.js
@@ -8,5 +8,35 @@ BillingCycle.methods(['get', 'post', 'put', 'delete'])
         new:true exige que devolva o novo registro e não o anterior*/
 BillingCycle.updateOptions({new:true, runValidators: true })
 
+BillingCycle.route('count',(req,res, next)=>{
+        BillingCycle.count((error,value) => {
+                if(error)
+                {
+                        res.status(500).json({errors: [error]})
+                } else 
+                {
+                        res.json({value})
+
+                }
+        })
+})
+
+BillingCycle.route('summary', (req, res, next)=> {
+        BillingCycle.aggregate({
+                $project: {credit: {$sum: "$credits.value"}, debt:{$sum:"$debts.value"}}
+        }, {
+                $group:  {_id:null,credit:{$sum:"$credit"},debt:{$sum: "$debt"}}
+        },{
+                $project:{_id:0,credit:1,debt:1}           
+           
+        },(error,result) => {
+                if (error){
+                        res.status(500).json({errors:[error]})
+                } else{
+                        res.json(result[0] || {credit:0, debt:0})
+                }
+        })
+})
+
 
 module.exports = BillingCycle
--- src/config/database.js
+++ src/config/database.js
@@ -5,3 +5,6 @@ module.exports = mongoose.connect('mongodb://localhost/mymoney')
 
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório"
+mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
+mongoose.Error.messages.Number.max = "O '{VALUE}' informado é maior que o limite máximo de '{MAX}'."
+mongoose.Error.messages.String.enum = "'{VLAUE}' não é válido para o atributo '{PATH}'."
